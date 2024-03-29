b738be4c2f031efeca9f1e393ed4c4851840e222
alessio picoco <alessio.picoco@gmail.com>
2024-03-29 20:53:13 +0100

    added jump
    my first commit !!!111!!1!!!111!1!

--- Assets/Scripts/Movement Component/PlayerComponent.cs
+++ Assets/Scripts/Movement Component/PlayerComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,42 +7,105 @@ using UnityEngine.InputSystem;
 
 public class PlayerComponent : MonoBehaviour
 {
-    private PlayerInputs input= null;
-    private float moveValue;
+    private PlayerInputs input = null;
 
-    [SerializeField] public float playerSpeed;
+    private float moveValue;
+    private float jumpValue;
+
+    [HideInInspector]
+    public bool isGrounded = true;
+    [HideInInspector]
+    public bool canJump = true;
+
+
+    Rigidbody2D rb; // player's rigid body
+
+    [Header("set the speed:")]
+    public float playerSpeed;
+
+    [Header("set jump hight:")]
+    public float jumpForce;
 
     private void Awake()
     {
         input = new PlayerInputs();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
         input.Enable();
-        input.Player.Movement.performed += OnMovementPerformed;
-        input.Player.Movement.performed += OnMovementCancelled;
+        input.Player.Movement.performed += OnHorizontal;
+        input.Player.Movement.canceled += OnHorizontalCancelled;
+        input.Player.Jump.started += OnJump;
+        input.Player.Jump.canceled += OnNotJumping;
     }
+
+
 
     private void OnDisable()
     {
+        input.Player.Movement.performed -= OnHorizontal;
+        input.Player.Movement.canceled -= OnHorizontalCancelled;
+        input.Player.Jump.started -= OnJump;
+        input.Player.Jump.canceled -= OnNotJumping;
         input.Disable();
-        input.Player.Movement.performed -= OnMovementPerformed;
-        input.Player.Movement.performed -= OnMovementCancelled;
+    }
+
+    private void Update()
+    {
+        if (input.Player.Jump.WasPressedThisFrame())
+            canJump = true;
+        else
+            canJump = false;
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(moveValue);
+        //Debug.Log(moveValue);
+
+        Vector2 Vel = Vector3.zero;
+        Vel.x = moveValue * playerSpeed * Time.fixedDeltaTime;
+
+        rb.AddForce(Vel, ForceMode2D.Impulse);
     }
 
-    private void OnMovementPerformed(InputAction.CallbackContext value)
+    private void OnHorizontal(InputAction.CallbackContext value)
     {
         moveValue = value.ReadValue<float>();
     }
 
-    private void OnMovementCancelled(InputAction.CallbackContext value)
+    private void OnHorizontalCancelled(InputAction.CallbackContext value)
     {
         moveValue = 0;
+    }
+
+    void OnJump(InputAction.CallbackContext value)
+    {
+        //if (isGrounded)
+        //{
+
+        rb.AddForce(jumpForce * Time.fixedDeltaTime * Vector2.up, ForceMode2D.Impulse);
+
+        isGrounded = false;
+        //}
+    }
+
+    private void OnNotJumping(InputAction.CallbackContext context)
+    {
+        jumpValue = 0f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            Onlanded();
+        }
+    }
+
+    void Onlanded()
+    {
+        isGrounded = true;
     }
 }
