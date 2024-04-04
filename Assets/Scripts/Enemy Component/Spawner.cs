86d003b345376596e232d757697b8a36394118ef
eloisa <simoneloisamae@gmail.com>
2024-04-05 01:21:13 +0200

    continuing enemies
    ninth commit1!1!

--- Assets/Scripts/Enemy Component/Spawner.cs
+++ Assets/Scripts/Enemy Component/Spawner.cs
@@ -12,13 +12,61 @@ public class Spawner : MonoBehaviour
 
     [HideInInspector] public bool isChoosingWeapon;
 
-    private void Update()
+    [HideInInspector] public List<GameObject> ranged = new List<GameObject>();
+    [HideInInspector] public List<GameObject> melee = new List<GameObject>();
+    [HideInInspector] public List<GameObject> mine = new List<GameObject>();
+
+    private void Awake()
     {
-        if (isChoosingWeapon)
+        foreach (Wave wave in waves)
         {
-            return;
+            foreach (Enemy enemy in wave.enemies)
+            {
+                SpawnEnemy(enemy);
+            }
         }
+    }
 
+    //private void Update()
+    //{
+    //    if (isChoosingWeapon)
+    //    {
+    //        return;
+    //    }
+    //}
 
+    void SpawnEnemy(Enemy enemy)
+    {
+        int ran = Random.Range(0, spawner.Length);
+
+        for (int i = 0; i < enemy.numberToSpawn; i++)
+        {
+             
+            
+            if (enemy.enemyPrefab.CompareTag("Ranged") && ranged.Count > 0)
+            {
+                ranged[0].SetActive(true);
+                ranged.RemoveAt(0);
+            }
+
+            else if (enemy.enemyPrefab.CompareTag("Melee") && melee.Count > 0)
+            {
+                melee[0].SetActive(true);
+                melee.RemoveAt(0);
+            }
+
+            else if (enemy.enemyPrefab.CompareTag("Mine") && mine.Count > 0)
+            {
+                mine[0].SetActive(true);
+                mine.RemoveAt(0);
+            }
+
+            else
+            {
+                Instantiate(enemy.enemyPrefab, spawner[ran].position, Quaternion.identity);
+            }
+
+            //Transform spawnedEnemy = Instantiate(enemy.enemyPrefab, spawner[ran].position, Quaternion.identity);
+        }
     }
 }
