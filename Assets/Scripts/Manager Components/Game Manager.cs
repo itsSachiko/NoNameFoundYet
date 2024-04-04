860e3f0e1151a84a94465403d370cac9478612a7
eloisa <simoneloisamae@gmail.com>
2024-04-04 03:08:33 +0200

    UI, menù finished, in game started
    seventh commit!1!1!

---
+++ Assets/Scripts/Manager Components/Game Manager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public UnityEvent play;
+    public UnityEvent options;
+    public UnityEvent credits;
+
+    public UnityEvent gameOver;
+    public UnityEvent win;
+}
