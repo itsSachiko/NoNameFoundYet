7e8ef9663907dca670dee1854d718b92f30087e0
emacrapa <emanuele.crapanzano02@gmail.com>
2024-04-09 21:16:18 +0200

    Revert "Merge branch 'manuu' of https://github.com/itsSachiko/NoNameFoundYet into manuu"
    This reverts commit e962ca6ac8fd1be1f8a6514a17ebc62d4ce346eb, reversing
    changes made to db219de3832a52053896736cd41ae58647733b4a.

--- Assets/BulletPool.cs
+++ Assets/BulletPool.cs
@@ -14,7 +14,7 @@ public class BulletPool : ScriptableObject
     private void OnDisable()
     {
         bullets.Clear();
-        bullets = new List<Transform>();
+        bullets = new();
     }
 
     public void GetBullet(Transform newBullet)
